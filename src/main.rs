c0ad42b6e5a25e94d1a06a7b3316ffe2d292750a
Sean Lau <lthon0225@gmail.com>
2022-08-29 00:03:18 +0800

    simple tuple sample

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,9 @@
 // mod print;
 // mod vars;
 // mod types;
-mod strings;
+// mod strings;
+mod tuples;
 
 fn main() {
-    // print::run();
-    // vars::run();
-    strings::run();
+    tuples::run();
 }
