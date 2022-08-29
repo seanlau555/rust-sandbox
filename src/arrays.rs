cd57ece3eeb60891b1b090fe8e6fffb4088f72ac
Sean Lau <lthon0225@gmail.com>
2022-08-29 22:39:55 +0800

    add arrays

---
+++ src/arrays.rs
@@ -0,0 +1,25 @@
+// Arrays - Fixed list where elements are the same data types
+
+use std::mem;
+
+pub fn run() {
+    let mut numbers: [i32; 5] = [1, 2, 3, 4, 5];
+
+    // Re-assign value
+    numbers[2] = 20;
+
+    print!("{:?}", numbers);
+
+    // Get single val
+    print!("Single value: {}", numbers[0]);
+
+    // Get array length
+    println!("Array Length: {}", numbers.len());
+
+    // Arrays are stack allocated
+    print!("Array occupies {} bytes", mem::size_of_val(&numbers));
+
+    // Get Slice
+    let slice: &[i32] = &numbers[0..2];
+    print!("Slice: {:?}", slice);
+}
